7c1419d0a6b6eef470a4da2979e4d635b63f09cb
SathishLiberty <sathish.v@libertymutual.com>
2022-02-07 19:16:01 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LayoutModule } from '@progress/kendo-angular-layout';
+import { PanelBarModule } from '@progress/kendo-angular-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { FormComponent } from './form/form.component';
+import { MenuModule } from '@progress/kendo-angular-menu';
+import { LabelModule } from '@progress/kendo-angular-label';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+
+
+
+
+
+import { TreeviewModule } from 'ngx-treeview';
+import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    LayoutModule,
+    PanelBarModule,
+    BrowserAnimationsModule,
+    MenuModule,
+    LabelModule,
+    DropDownsModule,
+    ButtonsModule,
+    TreeviewModule.forRoot(),
+    DateInputsModule,
+    InputsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
